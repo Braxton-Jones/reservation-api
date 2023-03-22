31a292a2184c0fcfa5e62079f586b1c0dc64eb59
Braxton <65457602+braxttsl-dev@users.noreply.github.com>
2023-03-22 17:03:15 -0400

    "changes: deployment fix"

--- server.js
+++ server.js
@@ -3,18 +3,19 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const cors = require('cors')
 const reservationRouter = require("./routes/reservations")
+const PORT = process.env.PORT
 const api = express()
 // Middleware
 api.use(
 	cors(),
 );
 api.use(express.json())
-api.use("/api/reservations/", reservationRouter)
+api.use(reservationRouter)
 
 // Connecting to Mongo DB
 mongoose.connect(process.env.MONGO_URI)
     .then(()=>{
-        api.listen(process.env.PORT, ()=>{
+        api.listen(PORT, ()=>{
             console.log('Connection is Up')
         });
     })
